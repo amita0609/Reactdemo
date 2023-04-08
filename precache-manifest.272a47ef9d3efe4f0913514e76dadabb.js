d92d7b4e7adbd8e799d0e7b9809840ffd9982cca
Amita Tiwari <at224217@gmail.com>
2023-04-08 08:18:59 +0530

    Updates

---
+++ precache-manifest.272a47ef9d3efe4f0913514e76dadabb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12d581de1992d0f88fcf",
+    "url": "/TextFormatting-ReactApp/static/css/main.ae118fa4.chunk.css"
+  },
+  {
+    "revision": "12d581de1992d0f88fcf",
+    "url": "/TextFormatting-ReactApp/static/js/main.12d581de.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/TextFormatting-ReactApp/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextFormatting-ReactApp/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "7af16bca69f69af76708",
+    "url": "/TextFormatting-ReactApp/static/js/runtime~main.7af16bca.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextFormatting-ReactApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c47a979f926898182b58757237a5460f",
+    "url": "/TextFormatting-ReactApp/index.html"
+  }
+];
